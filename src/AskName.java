186552379195ab24178ad42563168e49154c6ea2
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2023-10-06 11:06:37 +0300

    Update code

--- src/AskName.java
+++ src/AskName.java
@@ -2,4 +2,29 @@ import java.util.Scanner;
 
 public class AskName
 {
+    public static void main(String[] args)
+    {
+        // Create a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+
+        // Ask for the user's name
+        System.out.println("What is your name? ");
+
+        // Read the user's input as a String
+        String userName = in.nextLine();
+
+        // Check if the user provided a name
+        if (userName.isEmpty())
+        {
+            System.out.println("Error");
+        }
+        else
+        {
+            // Print the user's name
+            System.out.println("Your name is " + userName + ".");
+        }
+
+        // Close the scanner
+        in.close();
+    }
 }
