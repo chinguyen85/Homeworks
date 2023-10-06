c8724576c7a93c8bf7936446c1cd35155b0bffeb
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2023-10-06 12:30:30 +0300

    Add Java program that creates an aphorisms array & print the choice users made

---
+++ src/ArrayAphorisms.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayAphorisms {
+
+	public static void main(String[] args) {
+		String[] aphorisms = {
+	            "Actions speak louder than words.",
+	            "A barking dog never bites.",
+	            "A penny saved is a penny earned.",
+	            "All things come to those who wait."
+	        };
+		
+		// Ask users to choose a number
+		System.out.println("Pick number from 1-4.");
+		Scanner in = new Scanner(System.in);
+		int userInput = in.nextInt();
+		
+		// Print the corresponding aphorism
+		System.out.println(aphorisms[userInput]);
+
+	}
+
+}
