4c2bb187e742fe269795fa623347f08bd5d72dc8
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2023-10-06 11:19:16 +0300

    Add Java program to guess a name & count the time of guess

---
+++ src/GuessName.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessName {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int i = 0;
+        String nameGuess;
+        
+        do {
+            System.out.println("Guess my name (type stop to exit)");
+            nameGuess = in.nextLine();
+            i++;
+
+            if (nameGuess.equalsIgnoreCase("Emma")) {
+                System.out.println("Congratulations!");
+                break; // Exit the loop if the guess is correct
+            } else if (nameGuess.equalsIgnoreCase("stop")) {
+            	System.out.println("You guessed " + (i) + " times.");
+                break; // Exit the loop if the user wants to stop
+            }
+        } while (true);
+        if (!nameGuess.equalsIgnoreCase("stop")) {
+        	System.out.println("You guessed " + i + " times.");
+        }
+    }
+}
