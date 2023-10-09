e1b801d4561371058f99954084e2f4db07d0c469
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2023-10-09 13:48:08 +0300

    Re-arrange the code

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -2,26 +2,25 @@
 public class PrinterApp {
 
 	public static void main(String[] args) {
-		System.out.println("Printer App");
 		String creator = "Chi Nguyen";
-		System.out.println("This app is made by: " + creator);
-		
 		int number1 = 7;
 		int number2 = 3;
+		int multiplication = number1 * number2;
+		int addition = number1 + number2;
+		int division = number1 / number2;
+		int substraction = number1 - number2;
+		
+		System.out.println("Printer App");
+		System.out.println("This app is made by: " + creator);
+		
+		
 		System.out.println("The value of number1 is " + number1);
 		System.out.println("The value of number2 is " + number2);
 		
-		int multiplication = number1 * number2;
+		
 		System.out.println(number1 + " * " + number2 + " = " + multiplication);
-		
-		int addition = number1 + number2;
 		System.out.println(number1 + " + " + number2 + " = " + addition);
-		
-		int division = number1 / number2;
 		System.out.println(number1 + " / " + number2 + " = " + division);
-		
-		int substraction = number1 - number2;
 		System.out.println(number1 + " - " + number2 + " = " + substraction);
 	}
-
 }
